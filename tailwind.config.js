9a7af363e56f078d95c113f0ec8b3d174eba04e1
mjcarnaje <michaeljamescarnaje1@gmail.com>
2023-10-24 17:27:26 +0800

    feat: set inter as default font family in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   content: ["./website/templates/**/*.{html,js}", "./website/static/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
