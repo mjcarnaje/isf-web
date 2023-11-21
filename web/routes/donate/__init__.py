576e2f816e817fd730a3a19ca65971eb128b123a
joannagiga <giga.joanna123@gmail.com>
2023-11-21 10:04:42 +0800

    feat: donation page

--- web/routes/donate/__init__.py
+++ web/routes/donate/__init__.py
@@ -5,3 +5,13 @@ donate_bp = Blueprint("donate", __name__, url_prefix='/donate')
 @donate_bp.route('/', methods=['GET'])
 def index():
   return render_template('donate/donate.html')
+
+
+@donate_bp.route('/food_materials', methods=['GET'])
+def food_materials():
+    return render_template('donate/donateFood_materials.html')
+  
+  
+@donate_bp.route('/money', methods=['GET'])
+def money():
+    return render_template('donate/donateMoney.html')
