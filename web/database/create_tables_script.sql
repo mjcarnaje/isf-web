0fef3fb052381c7b2f8df403f5d2575338876ada
mjcarnaje <michaeljamescarnaje1@gmail.com>
2023-11-22 18:53:37 +0800

    feat: add animal

--- web/database/create_tables_script.sql
+++ web/database/create_tables_script.sql
@@ -7,3 +7,23 @@ CREATE TABLE IF NOT EXISTS admin (
     password VARCHAR(256) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TABLE IF NOT EXISTS animal (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    type VARCHAR(16) NOT NULL,
+    estimated_birth_month VARCHAR(16) NOT NULL,
+    estimated_birth_year VARCHAR(16) NOT NULL,
+    photo_url VARCHAR(256) NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    is_adopted BOOLEAN NOT NULL,
+    is_dead BOOLEAN NOT NULL,
+    is_dewormed BOOLEAN NOT NULL,
+    is_neutered BOOLEAN NOT NULL,
+    in_shelter BOOLEAN NOT NULL,
+    is_rescued BOOLEAN NOT NULL,
+    description TEXT,
+    appearance TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
---
+++ web/routes/rescue/admin_rescue.py
@@ -0,0 +1,68 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+
+from ...models import Animal
+from ...validations import AnimalValidation
+
+admin_rescue_bp = Blueprint("admin_rescue", __name__, url_prefix='/admin/rescue')
+
+@admin_rescue_bp.route('/', methods=['GET'])
+def index():
+    page = request.args.get('page', 1, type=int)
+    query = request.args.get('query', '', type=str)
+
+    animals_query = Animal.find_all(
+        page_number=page,
+        page_size=12,
+        query=query,
+    )
+
+    animals = animals_query.get("data")
+    has_previous_page = animals_query.get("has_previous_page")
+    has_next_page = animals_query.get("has_next_page")
+    total_count = animals_query.get("total_count")
+
+    return render_template('rescue/admin_rescues.html', animals=animals, has_previous_page=has_previous_page, has_next_page=has_next_page, total_count=total_count)
+
+@admin_rescue_bp.route('/add-animal', methods=['GET', 'POST'])
+def add_animal():
+  form = AnimalValidation()
+
+  if form.validate_on_submit():
+    name = form.name.data
+    type = form.type.data
+    estimated_birth_month = form.estimated_birth_month.data
+    estimated_birth_year = form.estimated_birth_year.data
+    photo_url = form.photo_url.data
+    gender = form.gender.data
+    is_adopted = form.is_adopted.data
+    is_dead = form.is_dead.data
+    is_dewormed = form.is_dewormed.data
+    is_neutered = form.is_neutered.data
+    in_shelter = form.in_shelter.data
+    is_rescued = form.is_rescued.data
+    description = form.description.data
+    appearance = form.appearance.data
+
+    animal_id = Animal.insert(
+      name=name,
+      type=type,
+      estimated_birth_month=estimated_birth_month,
+      estimated_birth_year=estimated_birth_year,
+      photo_url=photo_url,
+      gender=gender,
+      is_adopted=is_adopted,
+      is_dead=is_dead,
+      is_dewormed=is_dewormed,
+      is_neutered=is_neutered,
+      in_shelter=in_shelter,
+      is_rescued=is_rescued,
+      description=description,
+      appearance=appearance
+    )
+
+    if animal_id:
+       return redirect(url_for('admin_rescue.index'))
+
+
+  return render_template('rescue/admin_add_animal.html', form=form)
+
