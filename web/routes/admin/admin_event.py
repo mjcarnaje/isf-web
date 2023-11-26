ed6cadd74e2eda8f3510d35b6e62c36868e0be14
rhtupr <paulineruth.repe@g.msuiit.edu.ph>
2023-11-26 13:33:13 +0700

    feat: edit event

--- web/routes/admin/admin_event.py
+++ web/routes/admin/admin_event.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect, render_template, request, url_for
 
 from ...models import Event
 from ...utils import admin_required
-from ...validations import AddEventValidation
+from ...validations import AddEventValidation, EditEventValidation
 from flask_login import current_user
 
 admin_event_bp = Blueprint("event", __name__, url_prefix='/event')
@@ -12,6 +12,36 @@ admin_event_bp = Blueprint("event", __name__, url_prefix='/event')
 def index():
     events = Event.find_all()
     return render_template('event/admin_event_list.html', events=events.get('data'))
+
+@admin_event_bp.route('/<int:id>/edit', methods=['GET', 'POST'])
+@admin_required
+def edit_event(id):
+    event = Event.find_by_id(id)
+    form = EditEventValidation()
+
+    if form.validate_on_submit():
+        event.name=form.name.data
+        event.description=form.description.data
+        event.cover_photo_url=form.cover_photo_url.data
+        event.start_date=form.start_date.data
+        event.end_date=form.end_date.data
+        event.location=form.location.data
+        event.show_in_landing=form.show_in_landing.data
+        event.is_done=False    
+        Event.edit(event)
+        return redirect(url_for("admin.event.index"))
+    
+    if not form.is_submitted():
+        form.id.data = event.id
+        form.name.data = event.name
+        form.description.data = event.description
+        form.cover_photo_url.data = event.cover_photo_url
+        form.start_date.data = event.start_date.date()
+        form.end_date.data = event.end_date.date()
+        form.location.data = event.location
+        form.show_in_landing.data = event.show_in_landing == 1 
+    
+    return render_template('event/admin_event_edit.html', form=form)
 
 @admin_event_bp.route('/add-event', methods=['GET', 'POST'])
 @admin_required
--- web/validations/__init__.py
+++ web/validations/__init__.py
@@ -5,3 +5,4 @@ from .UserSignupValidation import UserSignupValidation
 from .AddDonationMoney import AddDonationMoney
 from .AddDonation_In_Kind import AddDonation_In_Kind
 from .AddEventValidation import AddEventValidation
+from .EditEventValidation import EditEventValidation 
