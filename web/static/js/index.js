76559a60b753472340f49aaa6e21a0b1620b5cba
mjcarnaje <michaeljamescarnaje1@gmail.com>
2023-11-30 01:18:11 +0800

    feat: add infinite scroll gallery, sticky navbar

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,1 +1,9 @@
-console.log("Hello, ISF-Project!");
+const scroller = document.querySelector(".scroller");
+const scrollerInner = document.querySelector(".scroller__inner");
+const scrollerContent = Array.from(scrollerInner.children);
+
+scrollerContent.forEach((item) => {
+  const duplicatedItem = item.cloneNode(true);
+  duplicatedItem.setAttribute("aria-hidden", true);
+  scrollerInner.append(duplicatedItem);
+});
